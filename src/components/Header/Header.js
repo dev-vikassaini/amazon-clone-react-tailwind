5eb424bfd7fed13c57d41f1e177c1bf10b0d831a
Vikas Kumar <Vikas.Kumar@adek.gov.ae>
2022-09-01 21:44:03 +0530

    amazon clone in tailwind

---
+++ src/components/Header/Header.js
@@ -0,0 +1,77 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+export default function Header() {
+  // eslint-disable-next-line no-script-url
+  const href = "javascript:void(0)";
+
+  return (
+    <React.Fragment>
+      <header className="w-full h-[99px] flex flex-col">
+        <div className="main-header w-full h-[60px] bg-black text-white flex flex-row justify-start">
+          <div className="w-[70%] flex flex-row">
+            <div className="logo-box flex flex-row">
+              <div className="w-[129px] h-[60px] flex flex-row pr-1 a-logo1 ml-3">
+                <a
+                  className="w-[129px] h-[50px] a-logo nav-logo-link"
+                  href={{ href }}
+                >
+                  <img
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                    className="logo h-[35px] logo-img"
+                    alt="Amazon Logo"
+                  />
+                </a>
+                <span className="-ml-[1.625rem] mt-3 text-sm">.in</span>
+              </div>
+            </div>
+            <div className="address-box flex flex-row ml-10 text-sm items-center w-44">
+              <div className="flex flex-row h-[52px] w-44 items-center my-[0.50rem] mx-[3px]">
+                <a href={href}>
+                  <div className="flex flex-row pr-2">
+                    <div className="flex flex-col">
+                      <span class="material-icons">location_on</span>
+                    </div>
+                    <div className="flex flex-col">
+                      <div className="text-[#ccc]">Hello</div>
+                      <div className="font-bold">Select your address</div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+
+            <div className="search-box flex flex-row w-[60%]">
+              <div className="flex flex-row items-center">
+                <div className="flex flex-row h-10 w-12 bg-[#f3f3f3] text-black rounded-l-[0.4rem] p-[6px]">
+                  <span>All</span>{" "}
+                  <span>
+                    <ArrowDropDownIcon />{" "}
+                  </span>
+                </div>
+                <input type="text" className="h-10 w-[27.3rem]" />
+                <div className="h-10 w-12 bg-[#febd69] text-black rounded-r-[0.4rem] p-[6px]">
+                  <SearchIcon class="items-center h-8" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            className="w-[30%] flex flex-row"
+            style={{ border: "1px solid white" }}
+          >
+            <div className="internalization-box">English</div>
+            <div className="account-list-box">Hello Sign in</div>
+            <div className="return-box">Returns</div>
+            <div className="cart-box">Cart</div>
+          </div>
+        </div>
+        <div className="sub-main-header h-[39px] bg-[#232f3e] text-white">
+          Hello Sub Header! 🤣
+        </div>
+      </header>
+    </React.Fragment>
+  );
+}
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDfO3plKh51RI3ueY3q9rSqVwfu--ryx8Y",
+  authDomain: "react-tailwind-a7674.firebaseapp.com",
+  projectId: "react-tailwind-a7674",
+  storageBucket: "react-tailwind-a7674.appspot.com",
+  messagingSenderId: "955512096457",
+  appId: "1:955512096457:web:95de79a0735347e931af18",
+  measurementId: "G-J0TKP07GX1"
+};
